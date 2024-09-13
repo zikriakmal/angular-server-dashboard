8303acb82da35b42a803f7a8e0afb241630bbaea
Zikri Akmal S <zikriakmale@gmail.com>
2024-09-13 13:31:23 +0700

    init: initial commit and finish implement component projection

---
+++ src/app/dashboard/dashboard-item/dashboard-item.component.ts
@@ -0,0 +1,17 @@
+import { Component, input, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './dashboard-item.component.html',
+  styleUrl: './dashboard-item.component.css'
+})
+export class DashboardItemComponent {
+  // @Input({ required: true }) image!: { src: string; alt: string };
+  // @Input({ required: true }) title!: string;
+  
+  // using signal
+  image = input.required<{src:string; alt: string}>();
+  title = input.required<string>();
+}
---
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { ButtonComponent } from '../../../shared/button/button.component';
+import { ControlComponent } from '../../../shared/control/control.component';
+
+@Component({
+  selector: 'app-new-ticket',
+  standalone: true,
+  imports: [ButtonComponent, ControlComponent],
+  templateUrl: './new-ticket.component.html',
+  styleUrl: './new-ticket.component.css'
+})
+export class NewTicketComponent {
+
+}
